fe4eb35b07324e8bee4a3a8a34fcd9f9b33c7b9e
AudranTourneur <108300467+AudranTourneur@users.noreply.github.com>
2023-11-30 15:52:55 +0100

    back

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,4 +1,30 @@
 #[macro_use] extern crate rocket;
+use bollard::Docker;
+use rocket::serde::{Serialize, Deserialize, json::Json};
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct MyVersion {
+    version: String,
+    api_version: String,
+}
+
+#[get("/version")]
+async fn get_version() -> Json<MyVersion> {
+    let docker: Docker = Docker::connect_with_local_defaults().unwrap();
+
+    let version_object = docker.version().await.unwrap();
+
+    let version_str = version_object.version.unwrap_or("UNDEFINED".to_string());
+    let api_version_str = version_object.api_version.unwrap_or("UNDEFINED".to_string());
+
+    let my_version = MyVersion {
+        version: version_str,
+        api_version: api_version_str,
+    };
+
+    return Json(my_version);
+}
 
 #[get("/hello/<name>/<age>")]
 fn hello(name: &str, age: u8) -> String {
@@ -7,5 +33,5 @@ fn hello(name: &str, age: u8) -> String {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+    rocket::build().mount("/", routes![get_version, hello])
 }
