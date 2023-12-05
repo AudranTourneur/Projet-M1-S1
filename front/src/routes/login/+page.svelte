93b92e6c990dec19aec07ce885c21d75e4c9347d
AudranTourneur <108300467+AudranTourneur@users.noreply.github.com>
2023-12-05 13:56:06 +0100

    skeleton setup

---
+++ front/src/routes/login/+page.svelte
@@ -0,0 +1,9 @@
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<h1 class="h1">Login</h1>
+        <p class="text-primary-500"> primary </p>
+        <p class="text-secondary-500"> secondary </p>
+        <p class="text-tertiary-500"> tertiary </p>
+		<a class="btn variant-filled-primary" href="https://kit.svelte.dev/">SvelteKit</a>
+	</div>
+</div>
