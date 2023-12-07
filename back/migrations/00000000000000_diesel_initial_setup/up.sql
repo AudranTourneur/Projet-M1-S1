32327ea1da0bd7d13bbd52a19fcd1a0451518952
AudranTourneur <108300467+AudranTourneur@users.noreply.github.com>
2023-12-07 13:05:46 +0100

    database

--- back/migrations/00000000000000_diesel_initial_setup/up.sql
+++ back/migrations/00000000000000_diesel_initial_setup/up.sql
@@ -15,14 +15,50 @@
 -- SELECT diesel_manage_updated_at('users');
 -- ```
 
-create table users (
+CREATE TABLE users (
     "id" SERIAL PRIMARY KEY,
     "username" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "salt" TEXT NOT NULL,
+    "topologoy" JSONB NOT NULL,
     "updated_at" TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
+
+CREATE TABLE sessions (
+    "session_token" TEXT PRIMARY KEY,
+    "user_id" INTEGER NOT NULL,
+    "expires_at" TIMESTAMP NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE container_statistics (
+    "id" TEXT,
+    "timestamp" TIMESTAMP NOT NULL,
+    "memory_usage" INTEGER NOT NULL, -- bytes
+    "cpu_usage" FLOAT NOT NULL, -- %
+    "io_usage_read" FLOAT NOT NULL, -- bytes/sec
+    "io_usage_write" FLOAT NOT NULL, -- bytes/sec
+    "network_usage_up" FLOAT NOT NULL, -- bytes/sec
+    "network_usage_down" FLOAT NOT NULL, -- bytes/sec
+    PRIMARY KEY ("id", "timestamp")
+);
+
+
+CREATE TABLE volume_statistics (
+    "id" TEXT,
+    "timestamp" TIMESTAMP NOT NULL,
+    "disk_usage" INTEGER NOT NULL, -- bytes
+    PRIMARY KEY ("id", "timestamp") 
+);
+
+CREATE TABLE network_statistics (
+    "id" TEXT,
+    "timestamp" TIMESTAMP NOT NULL,
+    "network_usage_up" FLOAT NOT NULL, -- bytes/sec
+    "network_usage_down" FLOAT NOT NULL, -- bytes/sec
+    PRIMARY KEY ("id", "timestamp") 
+);
 
 CREATE OR REPLACE FUNCTION diesel_manage_updated_at(_tbl regclass) RETURNS VOID AS $$
 BEGIN
