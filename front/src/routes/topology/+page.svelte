4147fe53b0c4e01def299b5a1d6f07290f0e7b18
AudranTourneur <108300467+AudranTourneur@users.noreply.github.com>
2023-12-07 23:03:59 +0100

    pixi topology

---
+++ front/src/routes/topology/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { initApp } from '$lib/topology';
+
+    let parentElement: HTMLDivElement;
+	let canvasElement: HTMLCanvasElement;
+
+	onMount(() => {
+		initApp(canvasElement, parentElement);
+	});
+</script>
+
+    <div bind:this={parentElement}>
+<canvas bind:this={canvasElement} id="topology-canvas" />
+        </div>
