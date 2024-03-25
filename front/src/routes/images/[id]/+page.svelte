76cca84283f8207b05cbd7afc94ce5accd93b394
Califano <amaury.giot@student.junia.com>
2024-03-25 11:23:11 +0100

    fix ubuntu rootfs mount

--- front/src/routes/images/[id]/+page.svelte
+++ front/src/routes/images/[id]/+page.svelte
@@ -12,7 +12,30 @@
     });
 </script>
 
+
+<div class="space-y-5">
+            <div class="relative p-3 m-2 bg-gray-800 shadow rounded-lg overflow-auto">
+                <div>
+                    <span>{image.id}</span>
+                </div>
+                <br/>
+                <div>
+                    <span>tags : {image.tags}</span>
+                </div>
+                <br/>
+                <div>
+                    Size :{formatBytes(image.size)}
+                </div>
+                <br/>
+                <div>
+                    <span>created: {image.created}  </span>
+                </div>
+
+
+            </div>
+        </div>
+
 <span>id: {image.id}  ||</span>
 <span>tags: {image.tags}  ||</span>
-<span>size: {image.size}  ||</span>
 <span>created: {image.created}</span>
+<span>size: {image.size}  ||</span>
