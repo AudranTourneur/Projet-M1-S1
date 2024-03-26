a6c624e31e680acf0ef3a13806667281e4991a2a
Simon de LAJARTRE <72396434+sljtre@users.noreply.github.com>
2024-03-26 20:49:54 +0100

    start ui page containers

--- front/src/routes/containers/+page.svelte
+++ front/src/routes/containers/+page.svelte
@@ -14,23 +14,27 @@
         console.log(`Downloading container with index ${index}`);
     }
 
-    function startContainer(index : number){
+    function startContainer(index: number) {
         // implémenter la fonction adéquate
         console.log(`Start a container with index ${index}`);
     }
 
-    function stopContainer(index : number){
+    function stopContainer(index: number) {
         //implémenter la fonction adéquate
         console.log(`Stop a container with index ${index}`);
     }
-
-
 </script>
 
 <div class="w-full">
+    normal que les containers soient dupliqués, je travaille dessus :-) Simon
     {#each containers as container, i}
-        <div class="border border-gray-300 rounded p-4 mb-4">
-            <h3 class="text-lg font-semibold mb-2">Containers Information</h3>
+        <div class="border-token border-surface-300-600-token rounded-container-token p-4 mb-4">
+            <div class="flex flex-col">
+                <span class="font-bold">{container.name[0].substring(1, container.name[0].length)}</span>
+                <span>{container.id.substring(0, 12)}...</span>
+            </div>
+        </div>
+        <div class="border-token border-surface-300-600-token rounded-container-token p-4 mb-4">
             <div class="flex justify-between items-center mb-2">
                 <span class="font-bold">ID:</span>
                 <span>{container.id}</span>
@@ -65,10 +69,10 @@
             </div>
             <div class="flex justify-end items-center">
                 <a href="/containers/{container.id}">
-                <button class="bg-blue-500 text-white px-4 py-2 rounded mr-2">
-                   Info
-                </button>
-                        </a>
+                    <button class="bg-blue-500 text-white px-4 py-2 rounded mr-2">
+                        Info
+                    </button>
+                </a>
                 <button class="bg-red-500 text-white px-4 py-2 rounded mr-2" on:click={() => deleteContainer(i)}>
                     Delete
                 </button>
