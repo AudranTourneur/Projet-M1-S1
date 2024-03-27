c6bf0492d731636ff21944e3b88c9104de1a2ec9
Califano <amaury.giot@student.junia.com>
2024-03-27 09:24:11 +0100

    stats containers

--- front/src/routes/images/[id]/+page.svelte
+++ front/src/routes/images/[id]/+page.svelte
@@ -10,32 +10,40 @@
     onMount(() => {
         console.log('todo');
     });
+
+    function formatBytes(bytes: number, decimals = 2): string {
+    if (!+bytes) return '0 Bytes'
+
+    const k = 1024
+    const dm = decimals < 0 ? 0 : decimals
+    const sizes = ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k))
+
+    return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
+    }
+
+    function formatCreatedDate(sec: number): string {
+        const date = new Date(sec * 1000);
+        const iso = date.toISOString();
+        return iso.split('.')[0].replace('T', ' ');
+    }
 </script>
 
-
-<div class="space-y-5">
-            <div class="relative p-3 m-2 bg-gray-800 shadow rounded-lg overflow-auto">
-                <div>
-                    <span>{image.id}</span>
-                </div>
-                <br/>
-                <div>
-                    <span>tags : {image.tags}</span>
-                </div>
-                <br/>
-                <div>
-                    Size :{formatBytes(image.size)}
-                </div>
-                <br/>
-                <div>
-                    <span>created: {image.created}  </span>
-                </div>
-
-
-            </div>
-        </div>
-
-<span>id: {image.id}  ||</span>
-<span>tags: {image.tags}  ||</span>
-<span>created: {image.created}</span>
-<span>size: {image.size}  ||</span>
+<div class="relative p-3 m-2 bg-gray-800 shadow rounded-lg overflow-auto">
+    <div>
+        <span>id: {image.id}</span>
+    </div>
+    <br/>
+    <div>
+        <span>tags: {image.tags}</span>
+    </div>
+    <br/>
+    <div>
+        <span>Created :{formatCreatedDate(image.created)}</span>
+    </div>
+    <br/>
+    <div>
+        Size :{formatBytes(image.size)}
+    </div>
+</div>
