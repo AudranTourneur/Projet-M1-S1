f6716d2a681171c5b8047357637a1a2f40e3d9e6
AudranTourneur <108300467+AudranTourneur@users.noreply.github.com>
2024-03-29 13:25:15 +0100

    icons

--- front/src/components/LeftNavigation.svelte
+++ front/src/components/LeftNavigation.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { getDrawerStore } from '@skeletonlabs/skeleton';
 	import Fa from 'svelte-fa';
-	import { faHouse, faImage, faBoxOpen, faCoins, faWifi, faChessBoard, faCog } from '@fortawesome/free-solid-svg-icons';
+	import { faHouse, faImage, faBoxOpen, faCoins, faWifi, faChessBoard, faCog, faCubes, faCube, faImages, faShareNodes } from '@fortawesome/free-solid-svg-icons';
 
 	const drawerStore = getDrawerStore();
 
@@ -13,11 +13,11 @@
 <nav class="list-nav p-4">
 	<ul class="flex gap-2 flex-col">
 		<li class="flex items-center"><Fa icon={faHouse} /><a class="fa-solid fa-house" on:click={drawerClose} href="/overview"> Overview</a></li>
-		<li class="flex items-center"><Fa icon={faImage} /><a class="fa-regular fa-image" on:click={drawerClose} href="/images"> Images</a></li>
-		<li class="flex items-center"><Fa icon={faBoxOpen} /><a class="fa-solid fa-box-open" on:click={drawerClose} href="/containers"> Containers</a></li>
+		<li class="flex items-center"><Fa icon={faImages} /><a class="fa-regular fa-image" on:click={drawerClose} href="/images"> Images</a></li>
+		<li class="flex items-center"><Fa icon={faCube} /><a class="fa-solid fa-box-open" on:click={drawerClose} href="/containers"> Containers</a></li>
 		<li class="flex items-center"><Fa icon={faCoins} /><a class="fa-solid fa-coins" on:click={drawerClose} href="/volumes">Volumes</a></li>
 		<li class="flex items-center"><Fa icon={faWifi} /><a class="fa-solid fa-wifi" on:click={drawerClose} href="/networks">Networks</a></li>
-		<li class="flex items-center"><Fa icon={faChessBoard} /><a class="fa-solid fa-chess-board" on:click={drawerClose} href="/topology">Topology</a></li>
+		<li class="flex items-center"><Fa icon={faShareNodes} /><a class="fa-solid fa-chess-board" on:click={drawerClose} href="/topology">Topology</a></li>
 		<li class="flex items-center"><Fa icon={faCog} /><a class="fa-solid fa-gear" on:click={drawerClose} href="/settings">Settings</a></li>
 	</ul>
 </nav>
--- front/src/routes/images/[id]/+page.svelte
+++ front/src/routes/images/[id]/+page.svelte
@@ -1,49 +1,57 @@
 <script lang="ts">
-    export let data;
+	export let data;
 
-    console.log(data);
+	console.log(data);
 
-    let image = data;
+	let image = data;
 
-    import { onMount } from 'svelte';
+	import { onMount } from 'svelte';
 
-    onMount(() => {
-        console.log('todo');
-    });
+	onMount(() => {
+		console.log('todo');
+	});
 
-    function formatBytes(bytes: number, decimals = 2): string {
-    if (!+bytes) return '0 Bytes'
+	function formatBytes(bytes: number | bigint, decimals = 2): string {
+		if (!+bytes) return '0 Bytes';
 
-    const k = 1024
-    const dm = decimals < 0 ? 0 : decimals
-    const sizes = ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
+		const k = 1024;
+		const dm = decimals < 0 ? 0 : decimals;
+		const sizes = ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
 
-    const i = Math.floor(Math.log(bytes) / Math.log(k))
+		const i = Math.floor(Math.log(bytes) / Math.log(k));
 
-    return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
-    }
+		return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
+	}
 
-    function formatCreatedDate(sec: number): string {
-        const date = new Date(sec * 1000);
-        const iso = date.toISOString();
-        return iso.split('.')[0].replace('T', ' ');
-    }
+	function formatCreatedDate(sec: number): string {
+		const date = new Date(sec * 1000);
+		const iso = date.toISOString();
+		return iso.split('.')[0].replace('T', ' ');
+	}
 </script>
 
 <div class="relative p-3 m-2 bg-gray-800 shadow rounded-lg overflow-auto">
-    <div>
-        <span>id: {image.id}</span>
-    </div>
-    <br/>
-    <div>
-        <span>tags: {image.tags}</span>
-    </div>
-    <br/>
-    <div>
-        <span>Created :{formatCreatedDate(image.created)}</span>
-    </div>
-    <br/>
-    <div>
-        Size :{formatBytes(image.size)}
-    </div>
+	<div>
+		<span>id: {image.id}</span>
+	</div>
+	<br />
+	<div>
+		<span>tags: {image.tags}</span>
+	</div>
+	<br />
+	<div>
+		<span>Created :{formatCreatedDate(image.created)}</span>
+	</div>
+	<br />
+	<div>
+		Size :{formatBytes(image.size)}
+	</div>
+	<div class="flex justify-end">
+		<a href="/images/{image.id}">
+			<button class="bg-blue-500 text-white px-4 py-2 rounded mr-2"> Info </button>
+		</a>
+		<button class="bg-red-500 text-white px-4 py-2 rounded mr-2" on:click={() => deleteVolume(i)}
+			>Delete</button
+		>
+	</div>
 </div>
