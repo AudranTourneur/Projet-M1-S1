2d965b1ee77737d7a3325efdfb4657522d658a65
abyuka <lecoqjudith@gmail.com>
2024-04-02 16:56:21 +0200

    i

---
+++ front/src/routes/volumes/FileExplorer.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+import {onMount} from "svelte";
+import {Base64} from "js-base64";
+
+export let id : string;
+let path = "/";
+let files = [];
+let directories = [];
+
+onMount(async () => {
+    const response = await fetch(`/volumes/${id}/filesystem/${Base64.encodeURI(path)}/api`);
+    const res = await response.json();
+    console.log(res);
+})
+
+
+
+</script>
+
+<!-- {#each files as file}
+    <div>{file.name}</div>
+{/each} -->
---
+++ front/src/routes/volumes/[id]/filesystem/[path]/api/+server.ts
@@ -0,0 +1,9 @@
+import type { RequestHandler } from "@sveltejs/kit";
+import {PUBLIC_API_URL} from '$env/static/public';
+import  {json} from '@sveltejs/kit'
+
+export const GET: RequestHandler = async ({params}) => {
+    const response = await fetch(PUBLIC_API_URL + '/volume/' + params.id + '/filesystem/' + params.path);
+    const serverResponseJson = await response.json();
+    return json(serverResponseJson);
+};
