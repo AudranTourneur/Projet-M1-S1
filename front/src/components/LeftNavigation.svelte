0a808dff469442ffeea305d932e0d09fb25344ed
Simon de LAJARTRE <72396434+sljtre@users.noreply.github.com>
2024-04-03 15:33:42 +0200

    improve menu ui and icons

--- front/src/components/LeftNavigation.svelte
+++ front/src/components/LeftNavigation.svelte
@@ -1,24 +1,48 @@
 <script lang="ts">
 	import { getDrawerStore } from '@skeletonlabs/skeleton';
+	import { page } from '$app/stores';
+
 	import Fa from 'svelte-fa';
-	import { faHouse, faImage, faBoxOpen, faCoins, faWifi, faChessBoard, faCog, faCubes, faCube, faImages, faShareNodes, faLayerGroup } from '@fortawesome/free-solid-svg-icons';
+	import {
+		faHouse,
+		faCoins,
+		faCog,
+		faCubes,
+		faImages,
+		faLayerGroup,
+		faNetworkWired,
+		faCircleNodes
+	} from '@fortawesome/free-solid-svg-icons';
 
 	const drawerStore = getDrawerStore();
+	$: currentPath = $page.url.pathname;
 
 	function drawerClose() {
 		drawerStore.close();
 	}
+
+	const menu = [
+		{ icon: faHouse, text: 'Overview', href: '/overview' },
+		{ icon: faImages, text: 'Images', href: '/images' },
+		{ icon: faCubes, text: 'Containers', href: '/containers' },
+		{ icon: faLayerGroup, text: 'Composes', href: '/composes' },
+		{ icon: faCoins, text: 'Volumes', href: '/volumes' },
+		{ icon: faNetworkWired, text: 'Networks', href: '/networks' },
+		{ icon: faCircleNodes, text: 'Topology', href: '/topology' },
+		{ icon: faCog, text: 'Settings', href: '/settings' }
+	];
 </script>
 
-<nav class="list-nav p-4">
-	<ul class="flex gap-2 flex-col">
-		<li class="flex items-center"><Fa icon={faHouse} /><a class="fa-solid fa-house" on:click={drawerClose} href="/overview">Overview</a></li>
-		<li class="flex items-center"><Fa icon={faImages} /><a class="fa-regular fa-image" on:click={drawerClose} href="/images">Images</a></li>
-		<li class="flex items-center"><Fa icon={faCube} /><a class="fa-solid fa-box-open" on:click={drawerClose} href="/containers">Containers</a></li>
-		<li class="flex items-center"><Fa icon={faLayerGroup} /><a class="fa-solid fa-box-open" on:click={drawerClose} href="/composes">Composes</a></li>
-		<li class="flex items-center"><Fa icon={faCoins} /><a class="fa-solid fa-coins" on:click={drawerClose} href="/volumes">Volumes</a></li>
-		<li class="flex items-center"><Fa icon={faWifi} /><a class="fa-solid fa-wifi" on:click={drawerClose} href="/networks">Networks</a></li>
-		<li class="flex items-center"><Fa icon={faShareNodes} /><a class="fa-solid fa-chess-board" on:click={drawerClose} href="/topology">Topology</a></li>
-		<li class="flex items-center"><Fa icon={faCog} /><a class="fa-solid fa-gear" on:click={drawerClose} href="/settings">Settings</a></li>
-	</ul>
+<nav class="flex gap-3 flex-col p-4">
+	{#each menu as item}
+		<a
+			on:click={drawerClose}
+			href={item.href}
+			class="btn items-center justify-start"
+			class:variant-soft-primary={currentPath === item.href}
+			class:variant-soft={currentPath !== item.href}>
+			<Fa icon={item.icon} />
+			<span>{item.text}</span>
+		</a>
+	{/each}
 </nav>
