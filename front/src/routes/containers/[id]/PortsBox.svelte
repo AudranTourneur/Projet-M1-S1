3e6efeb1a3620bf41297ed061633c09bb462961f
Califano <amaury.giot@student.junia.com>
2024-04-04 16:12:56 +0200

    container/id

--- front/src/routes/containers/[id]/PortsBox.svelte
+++ front/src/routes/containers/[id]/PortsBox.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { ContainerData } from "$lib/types/ContainerData";
+	import type { OurPortTypeEnum } from "$lib/types/OurPortTypeEnum";
 	import type { PortData } from "$lib/types/PortData";
 
     export let container: ContainerData;
@@ -7,6 +8,7 @@
     const c = container;
 
     let ports = c.ports.filter(x => x.ip?.includes('.'))
+    console.log(ports)
 
 
 	function addPort(): any {
@@ -19,24 +21,30 @@
 	}
 
 
-	function editPortType(): any {
-		throw new Error("Function not implemented.");
+    let newPortType: OurPortTypeEnum
+	function editPortType(port: PortData): any {
+        port.type = newPortType;
+        ports = ports;
 	}
 
 
-	function editPrivatePort(): any {
-		throw new Error("Function not implemented.");
+    let newPrivatePort: number
+	function editPrivatePort(port: PortData): any {
+        port.privatePort = newPrivatePort;
+        ports = ports;
 	}
 
 
-    function editPortIP(): any {
-		throw new Error("Function not implemented.");
+    let newPortIP = '';
+    function editPortIP(port: PortData): any {
+        port.ip = newPortIP;
+        ports = ports;
 	}
 
 
+    let newPublicPort: number
 	function editPublicPort(port: PortData): any {
-		console.log(port)
-        port.publicPort++
+        port.publicPort = !! newPublicPort ? newPublicPort:0;
         ports = ports;
 	}
 </script>
@@ -46,10 +54,17 @@
 {#each ports as port}
     Host port {port.publicPort} on {port.ip} is binded to the internal port {port.privatePort} for the protocol {port.type}
         <button class="btn variant-glass-primary" on:click={() => editPublicPort(port)}>Edit public port</button>
-        <input bind:value={port.publicPort} placeholder="enter the new port" />
-        <button class="btn variant-glass-primary" on:click={() => editPortIP()}>Edit port IP</button>
-        <button class="btn variant-glass-primary" on:click={() => editPrivatePort()}>Edit private port</button>
-        <button class="btn variant-glass-primary" on:click={() => editPortType()}>Edit port type</button>
+        <input title="Input (number)" type="number" bind:value={port.publicPort} placeholder="new public port" />
+
+        <button class="btn variant-glass-primary" on:click={() => editPortIP(port)}>Edit port IP</button>
+        <input title="Input (text)" type="text" bind:value={newPortIP} placeholder="new IP" />
+
+        <button class="btn variant-glass-primary" on:click={() => editPrivatePort(port)}>Edit private port</button>
+        <input title="Input (number)" type="number" bind:value={newPrivatePort} placeholder="new private port" />
+
+        <button class="btn variant-glass-primary" on:click={() => editPortType(port)}>Edit port type</button>
+        <input title="Input (text)" type="text" bind:value={newPortType} placeholder="new port type" />
+
         <button class="btn variant-glass-primary" on:click={() => deleteBiding()}>delete biding</button>
         <br>
 {/each}
