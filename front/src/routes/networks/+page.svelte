79385dc07bc51a535dfe7f9948e171cd7f540af3
antoinefournet <antoine.fournet@student.junia.com>
2024-04-08 15:52:46 +0200

    Edit Network page & Info Network Page

--- front/src/routes/networks/+page.svelte
+++ front/src/routes/networks/+page.svelte
@@ -3,7 +3,11 @@
 	console.log('data=', data);
 
 	const networks = data.networks;
-	let search = '';
+	let search = ''
+
+    let visibleNetworks = [...networks];
+    const images = data.images;
+
 	// let response = JSON.parse(data)
 	// console.log(response);
 
@@ -20,71 +24,92 @@
 	}
 </script>
 
-<h1 class="text-center text-4xl">Networks</h1>
 
+<!-- <h1 class="text-center text-4xl">Networks</h1>
+<br />
+<div class="mx-auto max-w-xs">
+	<input bind:value={search} type="text" placeholder="Search by name or ID" class="input mb-4" /> ### à modifier
+</div>
+--> 
 {#each networks as network}
-	{network.name} <br />
-	<div class="space-y-5">
-		<div class="relative p-3 m-2 shadow rounded-lg overflow-auto">
-			<div>
-				<span>{network.name}</span>
-			</div>
-			<br />
-			<div>
-				<span>ID : {network.id}</span>
-			</div>
-			<br />
-			<div>
-				<span>Created :{network.created}</span>
-			</div>
-			<br />
-			<div>
-				Labels :
-                <br />
-                {#each Object.entries(network.labels) as [str1, str2]}
-                {str1}    :    {str2}
-                <br />
-                {/each}
-			</div>
-			<br />
-			<div>
-                Ipam Config :
-				{#each network.ipamConfig as config}
-					<div>
-						<p>Subnet: {config.subnet}</p>
-						<p>IP Range: {config.ipRange}</p>
-						<p>Gateway: {config.gateway}</p>
-						{#if config.auxAddresses}
-							<p>Aux Addresses:</p>
-							<ul>
-								{#each Object.entries(config.auxAddresses) as [key, value]}
-									<li>{key}: {value}</li>
-								{/each}
-							</ul>
-						{/if}
-					</div>
-				{/each}
-			</div>
-            <br />    
+    <br />
+    <div class="flex justify-between items-start gap-2 p-3 rounded-container-token overflow-auto bg-surface-300/30 dark:bg-surface-600/30 shadow border-token border-surface-300-600-token">
+        <div class="flex flex-col">
             <div>
-                Containers :
-                {#each Object.entries(network.containers) as [name,other]}
-                <div>
-                    <p>Name: {other.name}</p>
-                    <p>Endpoint Id: {other.endpointId}</p>
-                    <p>Mac Address: {other.macAddress}</p>
-                    <p>IPv4 Address: {other.ipv4Address}</p>
-                    <p>IPv6 Address: {other.ipv6Address}</p>
-                    <br />
-                </div>
-                {/each}
+                <span><u>{network.name ? network.name : 'none'}</u></span>
             </div>
             <br />
-			<div class="flex justify-end">
-				<a href="/networks/{network.id}">
-					<button class="bg-blue-500 text-white px-4 py-2 rounded mr-2"> Info </button>
-				</a>
-			</div>
-		</div>
-	</div>
+            <div>
+                <span><b>ID :</b> {network.id ? network.id.substring(0, 19) + '...' : 'none'}</span>
+            </div>
+            <br />
+            <div>
+                <span><b>Created :</b> {network.created ? network.created : 'none'}</span>
+            </div>
+        </div>
+        <div class="flex flex-col">
+            <div>
+                <b>Labels :</b>
+                <br />
+                {#if network.labels}
+                    {#each Object.entries(network.labels) as [str1, str2]}
+                        {str1} : {str2}
+                        <br />
+                    {/each}
+                {:else}
+                    none
+                {/if}
+            </div>
+        </div>
+        <div class="flex flex-col">
+            <div>
+                <b>Ipam Config :</b>
+                {#if network.ipamConfig}
+                    {#each network.ipamConfig as config}
+                        <p>Subnet: {config.subnet ? config.subnet : 'none'}</p>
+                        <p>IP Range: {config.ipRange ? config.ipRange : 'none'}</p>
+                        <p>Gateway: {config.gateway ? config.gateway : 'none'}</p>
+                        {#if config.auxAddresses}
+                            <p>Aux Addresses:</p>
+                            <ul>
+                                {#each Object.entries(config.auxAddresses) as [key, value]}
+                                    <li>{key}: {value}</li>
+                                {/each}
+                            </ul>
+                        {/if}
+                    {/each}
+                {:else}
+                    none
+                {/if}
+            </div>
+        </div>
+        <div class="flex flex-col">
+            <div>
+                <b>Containers :</b>
+                {#if network.containers}
+                    <div class="mt-5">
+                       <div class="grid grid-cols-1 gap-2">
+                            {#each Object.entries(network.containers) as [id, c]}
+                                <div class="flex items-center gap-4 border border-1 p-2 px-4 rounded-lg">
+                                    <div class="flex flex-col">
+                                        <a href="/containers/{id}" class="text-xl font-semibold hover:text-surface-500">{c.name}</a>
+                                        <span class="text-surface-600-300-token">{c.ipv4Address}</span>
+                                        <span class="text-surface-600-300-token">{c.macAddress}</span>
+                                    </div>
+                                    <br />
+                                </div>
+                            {/each}
+                        </div>
+                    </div>
+                {:else}
+                    none
+                {/if}
+            </div>
+        </div>
+        <div class="flex justify-end">
+            <a href="/networks/{network.id}">
+                <button class="bg-blue-500 text-white px-4 py-2 rounded mr-2"> Info </button>
+            </a>
+        </div>
+    </div>
 {/each}
--- front/src/routes/networks/[id]/+page.svelte
+++ front/src/routes/networks/[id]/+page.svelte
@@ -1,80 +1,105 @@
+
 <script lang="ts">
 	import { faCube } from '@fortawesome/free-solid-svg-icons';
 	import Fa from 'svelte-fa';
-
+ 
+ 
 	export let data;
-</script>
-
-<h1 class="text-center text-4xl">{data.name}</h1>
-
-<div class="space-y-5">
-	<div class="relative p-3 m-2 shadow rounded-lg overflow-auto">
-		<div>
-			<span>{data.name}</span>
-		</div>
-		<br />
-		<div>
-			<span>ID : {data.id}</span>
-		</div>
-		<br />
-		<div>
-			<span>Created :{data.created}</span>
-		</div>
-		<br />
-		<div>
-			Labels :
-			<br />
-			{#each Object.entries(data.labels) as [str1, str2]}
-				{str1} : {str2}
-				<br />
-			{/each}
-		</div>
-		<br />
-		<div>
-			Ipam Config :
-			{#each data.ipamConfig as config}
-				<div>
-					<p>Subnet: {config.subnet}</p>
-					<p>IP Range: {config.ipRange}</p>
-					<p>Gateway: {config.gateway}</p>
-					{#if config.auxAddresses}
-						<p>Aux Addresses:</p>
-						<ul>
-							{#each Object.entries(config.auxAddresses) as [key, value]}
-								<li>{key}: {value}</li>
-							{/each}
-						</ul>
-					{/if}
+ </script>
+ 
+ 
+ <style>
+	.box-wrapper {
+		padding: 1rem; 
+		margin: 1rem; 
+		border-radius: 8px;
+		border: 1px solid #fff;
+	}
+ 
+ 
+	.item {
+		padding: 0.5rem;
+	}
+ 
+ 
+	.grid-cols-1 {
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-gap: 1rem;
+	}
+ 
+ 
+	.grid-cols-2 {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		grid-gap: 1rem;
+	}
+ 
+ 
+	.underline {
+		text-decoration: underline;
+	}
+ </style>
+ 
+ 
+ <h1 class="text-center text-4xl">{data.name}</h1>
+ 
+ 
+ <div class="space-y-5">
+	<div class="box-wrapper relative overflow-auto">
+		<div class="{Object.keys(data).length > 2 ? 'grid-cols-2' : 'grid-cols-1'}">
+			{#if data.id}
+				<div class="item title">
+					<span class="underline">ID :</span> {data.id}
 				</div>
-			{/each}
-		</div>
-		<br />
-		<div>
-			Containers :
-			<!-- {#each Object.entries(data.containers) as [name, other]}
-				<div>
-					<p>Name: {other.name}</p>
-					<p>Endpoint Id: {other.endpointId}</p>
-					<p>Mac Address: {other.macAddress}</p>
-					<p>IPv4 Address: {other.ipv4Address}</p>
-					<p>IPv6 Address: {other.ipv6Address}</p>
-					<br />
+			{/if}
+			{#if data.created}
+				<div class="item title">
+					<span class="underline">Created :</span><br>
+					<span>{data.created}</span>
 				</div>
-			{/each} -->
-
-			<div class="grid grid-cols-4 gap-4">
-				{#each Object.entries(data.containers) as [id, c]}
-					<div class="flex items-center gap-4 border border-1 p-2 px-4 rounded-lg">
-						<Fa icon={faCube} size="2.5x"></Fa>
-						<div class="flex flex-col">
-							<a href="/containers/{id}" class="text-xl font-semibold hover:text-surface-500">{c.name}</a>
-							<span class="text-surface-600-300-token">{c.ipv4Address}</span>
-							<span class="text-surface-600-300-token">{c.macAddress}</span>
-						</div>
-						<br />
-					</div>
+			{/if}
+			<div class="item title">
+				<span class="underline">Labels :</span>
+				{#each Object.entries(data.labels) as [str1, str2]}
+					<div>{str1} : {str2}</div>
 				{/each}
 			</div>
+			{#if data.ipamConfig && data.ipamConfig.length > 0}
+				<div class="item title">
+					<span class="underline">Ipam Config :</span>
+					{#each data.ipamConfig as config}
+						{#if Object.keys(config).length > 0}
+							<div>
+								{#each Object.entries(config) as [key, value]}
+									{#if value}
+										<p>{key}: {value}</p>
+									{/if}
+								{/each}
+							</div>
+						{/if}
+					{/each}
+				</div>
+			{/if}
 		</div>
 	</div>
-</div>
+ 
+ 
+	<div class="mt-5">
+		Containers :
+		<div class="grid grid-cols-4 gap-4">
+			{#each Object.entries(data.containers) as [id, c]}
+				<div class="flex items-center gap-4 border border-1 p-2 px-4 rounded-lg">
+					<Fa icon={faCube} size="2.5x"></Fa>
+					<div class="flex flex-col">
+						<a href="/containers/{id}" class="text-xl font-semibold hover:text-surface-500">{c.name}</a>
+						<span class="text-surface-600-300-token">{c.ipv4Address}</span>
+						<span class="text-surface-600-300-token">{c.macAddress}</span>
+					</div>
+					<br />
+				</div>
+			{/each}
+		</div>
+	</div>
+ </div>
+ 
