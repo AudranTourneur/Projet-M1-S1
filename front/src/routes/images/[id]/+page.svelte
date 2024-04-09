b36dd2e41cca6f5fa12c852ead1a6aafc3edec7f
Simon de LAJARTRE <72396434+sljtre@users.noreply.github.com>
2024-04-09 17:17:05 +0200

    enable remove image on its page

--- front/src/routes/images/[id]/+page.svelte
+++ front/src/routes/images/[id]/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
-	import { formatBytes, formatCreatedDate } from '$lib/FormatUtils';
+	import { formatBytes } from '$lib/FormatUtils';
+	import Fa from 'svelte-fa';
+	import { faCircleNotch, faPlusCircle, faTrash } from '@fortawesome/free-solid-svg-icons';
+	import { goto } from '$app/navigation';
 
 	export let data;
 
@@ -7,15 +10,13 @@
 
 	let image = data;
 
-	import { onMount } from 'svelte';
-
-	onMount(() => {
-		console.log('todo');
-	});
-
 	let name = '';
-	async function CreateNewContainer() {
-		const res = await fetch('/images/' + data.id + '/api/create-container', {
+	let isLoadingCreate = false;
+	let isLoadingRemove = false;
+
+	const CreateNewContainer = async () => {
+		isLoadingCreate = true;
+		await fetch('/images/' + data.id + '/api/create-container', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
@@ -24,13 +25,17 @@
 				containerName: !!name ? name : null
 			})
 		});
-		console.log(res);
-	}
+		isLoadingCreate = false;
+	};
 
-
-	function deleteVolume(i: any): any {
-		throw new Error('Function not implemented.');
-	}
+	const deleteVolume = async () => {
+		isLoadingRemove = true;
+		await fetch(`/images/${image.id}/api/remove`, {
+			method: 'POST'
+		});
+		isLoadingRemove = false;
+		goto('/images');
+	};
 </script>
 
 <div class="relative p-3 m-2 shadow rounded-lg overflow-auto">
@@ -43,22 +48,23 @@
 	</div>
 	<br />
 	<div>
-		<span>Created :{formatCreatedDate(image.created)}</span>
+		<span>Created : {new Date(image.created).toLocaleString()}</span>
 	</div>
 	<br />
 	<div>
 		Size :{formatBytes(image.size)}
 	</div>
-	<div class="flex justify-end">
-		<input class="input" bind:value={name} placeholder="enter your name" />
-		<button class="btn variant-ghost-success p-2" on:click={CreateNewContainer}>
-			Create a New Container
+	<div class="flex justify-end mt-4">
+		<input class="input" bind:value={name} placeholder="Container name" />
+		<button class="btn variant-ghost-success ml-2" on:click={CreateNewContainer} disabled={isLoadingCreate}>
+			<Fa icon={isLoadingCreate ? faCircleNotch : faPlusCircle} spin={isLoadingCreate} fw class="mr-1" />
+			Create a new container from this image
 		</button>
-
-		<a href="/images/{image.id}">
-			<button class="bg-blue-500 text-white px-4 py-2 rounded mr-2"> Info </button>
-		</a>
-		<button class="bg-red-500 text-white px-4 py-2 rounded mr-2" on:click={() => deleteVolume(i)}
-			>Delete</button>
+	</div>
+	<div class="flex justify-end mt-4">
+		<button class="btn variant-filled-error ml-6" on:click={deleteVolume} disabled={isLoadingRemove}>
+			<Fa icon={isLoadingRemove ? faCircleNotch : faTrash} spin={isLoadingRemove} fw class="mr-1" />
+			Delete image
+		</button>
 	</div>
 </div>
