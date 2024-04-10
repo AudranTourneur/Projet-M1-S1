5ffe1ff6fe8001163d90eee0749f84afca03c18d
xavier <xavier@Ubuntu2.myguest.virtualbox.org>
2024-04-10 16:52:03 +0200

    En cours de modif

--- front/src/routes/volumes/[id]/api/change-name/+server.ts
+++ front/src/routes/volumes/[id]/api/change-name/+server.ts
@@ -1,1 +1,17 @@
-//init  
+import { PUBLIC_API_URL } from '$env/static/public';
+import type { RequestHandler } from '@sveltejs/kit';
+
+
+export const POST: RequestHandler = async ({ params, fetch }) => {
+    const name = params.name;
+    const response = await fetch(`${PUBLIC_API_URL}/volume/${name}/changeName`, {
+        method: 'POST',
+      });
+    
+      if (!response.ok) {
+        const errorMessage = `Failed to change volume name ${name}`;
+        throw new Error(errorMessage);
+      }
+    
+      return json({ message: `Volume ${name} changed name.` });
+    };
