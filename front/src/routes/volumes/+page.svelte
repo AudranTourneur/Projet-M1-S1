21a12a6414b3a59e18f59fd5c874fd4d066d9a8c
xavier <xavier@Ubuntu2.myguest.virtualbox.org>
2024-04-16 10:49:16 +0200

    Ajout de la fonction recherche de volumes

--- front/src/routes/volumes/+page.svelte
+++ front/src/routes/volumes/+page.svelte
@@ -4,13 +4,22 @@
 
     export let data;
 
-    const volumes : VolumeData[] = data.volumes;
+    const volumes: VolumeData[] = data.volumes;
+    let visibleVolumes = [...volumes];
+    let search = '';
 
-    console.log(volumes);
+
+    function searchVolumes() {
+        visibleVolumes = volumes.filter(volume => volume.name.toLowerCase().includes(search.toLowerCase()));
+    }
 </script>
 
+<h1 class="text-center text-4xl mb-5">Volumes</h1>
+<div class="mx-auto max-w-xs">
+    <input bind:value={search} type="text" placeholder="Search by name" class="input mb-4" on:input={searchVolumes} />
+</div>
 <div class="w-full">
-    {#each volumes as volume, i}
+    {#each visibleVolumes as volume, i}
         <VolumeBox volume={volume} />
     {/each}
 </div>
