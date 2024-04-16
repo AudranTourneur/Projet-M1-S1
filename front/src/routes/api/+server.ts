6e40f50a7e796b131f566a8424782b57fc5c4408
abyuka <lecoqjudith@gmail.com>
2024-04-16 11:42:06 +0200

    api logout

---
+++ front/src/routes/api/+server.ts
@@ -0,0 +1,37 @@
+import { json, text } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { PUBLIC_API_URL } from '$env/static/public';
+
+
+export const POST = async ({ request, cookies }) => {
+    try {
+        const reqJson = await request.json();
+
+        const apiRes = await fetch(PUBLIC_API_URL + '/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                action: "disconnect"
+            })
+        });
+
+        cookies.set('auth', '', {
+            expires: new Date(0),
+            path: '/',
+        });
+        return json({
+            success: true
+        })
+
+
+    } catch (e) {
+        console.error(e);
+    }
+
+    return json({
+        success: false,
+        message: 'Failed to logout'
+    })
+}
