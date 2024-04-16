da1b361cc1921a666f5d8b506001240629c209c5
AudranTourneur <108300467+AudranTourneur@users.noreply.github.com>
2024-04-16 14:08:54 +0200

    updated front dependencies

--- front/src/routes/images/[id]/+page.server.ts
+++ front/src/routes/images/[id]/+page.server.ts
@@ -2,7 +2,7 @@ import type { PageServerLoad } from './$types';
 import { PUBLIC_API_URL } from '$env/static/public';
 import type { ImageData } from '$lib/types/ImageData';
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ params, fetch }) => {
 	const { id } = params;
 	const serverResponse = await fetch(PUBLIC_API_URL + '/image/' + id);
 	const res = (await serverResponse.json()) as ImageData;
