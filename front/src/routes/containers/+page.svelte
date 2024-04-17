cdc34e5150b86ae5e88cde369ad5af63dc19ec7f
Simon de LAJARTRE <72396434+sljtre@users.noreply.github.com>
2024-04-17 15:22:58 +0200

    update containers

--- front/src/routes/containers/+page.svelte
+++ front/src/routes/containers/+page.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
-
     import ContainerBox from "./ContainerBox.svelte";
+    import Fa from 'svelte-fa';
+    import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
+    import { onMount } from 'svelte';
 
     export let data;
     let containers = data.containers;
+
+    const refetchInterval = 10000;
+    let interval: number;
 
     const refetchContainers = async () => {
         await fetch("/containers/api/list")
@@ -12,10 +17,20 @@
                 containers = data.containers;
             });
     }
+
+    onMount(()=>{
+        if (interval) clearInterval(interval);
+        interval = setInterval(refetchContainers, refetchInterval);
+    } );
+
 </script>
 
-<div class="w-full">
+<div class="flex items-center justify-center gap-2 text-surface-400-500-token text-sm">
+    <Fa icon={faCircleNotch} spin size="xs" />
+    Live data, updated every 10 seconds
+</div>
+{#key containers}
     {#each containers as container}
         <ContainerBox {container} refresh={refetchContainers} />
     {/each}
-</div>
+{/key}
