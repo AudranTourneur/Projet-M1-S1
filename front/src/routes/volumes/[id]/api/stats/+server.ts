595bee320b7366358c7eb0f2ae858c996870d244
abyuka <lecoqjudith@gmail.com>
2024-04-18 23:31:50 +0200

    undefined for GET /statistics-historical/volume/ [undefined]

--- front/src/routes/volumes/[id]/api/stats/+server.ts
+++ front/src/routes/volumes/[id]/api/stats/+server.ts
@@ -4,8 +4,7 @@ import { json } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async ({ params, fetch }) => {
 	const {encoded_path} = params;
-	console.log("encoded_path sent to api", encoded_path);
-	const response = await fetch(BACKEND_API_URL + "/statistics-historical/volume/<encoded_path>" + encoded_path);
+	const response = await fetch(BACKEND_API_URL + "/statistics-historical/volume/" + encoded_path);
 	const res = await response.json();
 	return json(res);
 };
